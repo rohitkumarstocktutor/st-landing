adf887db4630c3a99d13725d4836294aaabfcc31
rohit kumar <rohit.kumar@aisensy.com>
2025-11-05 22:49:01 +0530

    added verismart form

--- app/ayushi/page.tsx
+++ app/ayushi/page.tsx
@@ -293,7 +293,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1834827303964862');
+fbq('init', '4050173501915831');
 fbq('track', 'PageView');
 
         `}
@@ -303,7 +303,7 @@ fbq('track', 'PageView');
                     height="1"
                     width="1"
                     style={{ display: 'none' }}
-                    src="https://www.facebook.com/tr?id=1834827303964862&ev=PageView&noscript=1"
+                    src="https://www.facebook.com/tr?id=4050173501915831&ev=PageView&noscript=1"
 
                 />
             </noscript>
