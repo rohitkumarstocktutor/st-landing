02ca5cf2b9bf3527a5e6c37df9e56e6cead2fc4b
rohit kumar <rohit.kumar@aisensy.com>
2025-11-22 07:09:08 +0530

    fea: new page for ayushi forex trading

--- app/ayushi-forex-masterclass/page.tsx
+++ app/ayushi-forex-masterclass/page.tsx
@@ -344,9 +344,9 @@ function Page() {
         let redirectUrl = "";
 
         if (hostname.includes("chahataggrawal.in")) {
-            redirectUrl = "https://stocktutor.chahataggrawal.in/ayushi-forex-trading/thankyou";
+            redirectUrl = "https://stocktutor.chahataggrawal.in/ayushi-forex-masterclass/thankyou";
         } else {
-            redirectUrl = "https://stocktutor.co/ayushi-forex-trading/thankyou";
+            redirectUrl = "https://stocktutor.co/ayushi-forex-masterclass/thankyou";
         }
 
         const data = {
@@ -405,7 +405,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1834827303964862');
+fbq('init', '709734288843766');
 fbq('track', 'PageView');
 
         `}
@@ -415,7 +415,7 @@ fbq('track', 'PageView');
                     height="1"
                     width="1"
                     style={{ display: 'none' }}
-                    src="https://www.facebook.com/tr?id=1834827303964862&ev=PageView&noscript=1"
+                    src="https://www.facebook.com/tr?id=709734288843766&ev=PageView&noscript=1"
 
                 />
             </noscript>
--- app/ayushi-forex-masterclass/thankyou/page.tsx
+++ app/ayushi-forex-masterclass/thankyou/page.tsx
@@ -42,7 +42,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1834827303964862');
+fbq('init', '709734288843766');
 fbq('track', 'PageView');
 
         `}
@@ -52,7 +52,7 @@ fbq('track', 'PageView');
           height="1"
           width="1"
           style={{ display: 'none' }}
-          src="https://www.facebook.com/tr?id=1834827303964862&ev=PageView&noscript=1"
+          src="https://www.facebook.com/tr?id=709734288843766&ev=PageView&noscript=1"
 
         />
       </noscript>
