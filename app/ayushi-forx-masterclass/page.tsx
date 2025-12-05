3ae41619f68cd621915e8c9b39c7ce59ebfb9527
rohit kumar <rohit.kumar@aisensy.com>
2025-12-05 10:11:19 +0530

    ad level data

--- app/ayushi-forx-masterclass/page.tsx
+++ app/ayushi-forx-masterclass/page.tsx
@@ -397,6 +397,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
+fbq('set', 'autoConfig', false, '709734288843766');
 fbq('init', '709734288843766');
 fbq('track', 'PageView');
 
--- app/ayushi-forx-masterclass/thankyou/page.tsx
+++ app/ayushi-forx-masterclass/thankyou/page.tsx
@@ -42,8 +42,10 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
+fbq('set', 'autoConfig', false, '709734288843766');
 fbq('init', '709734288843766');
 fbq('track', 'PageView');
+fbq('track', 'Lead');
 
         `}
       </Script>
